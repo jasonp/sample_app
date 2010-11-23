5bd7a180a070f18d210def78d0a597d9136d543b
Jason Preston <jason@prestons.me>
2010-11-22 21:30:13 -0800

    Added an about page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe PagesController do
+	render_views
 
   describe "GET 'home'" do
     it "should be successful" do
@@ -14,6 +15,13 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+  end
+  
+  describe "GET 'about'" do
+  	it "should be successful" do
+  		get 'about'
+  		response.should be_success
+  	end
   end
 
 end
